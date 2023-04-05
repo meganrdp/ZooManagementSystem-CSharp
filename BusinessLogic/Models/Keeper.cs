1576bad835a479a2a1923e934e160f08bb9a029f
Mike Collingwood <mike.collingwood@softwire.com>
2023-04-05 08:45:46 +0000

    ACADEMY22-469: Add ZooManagement-C#

---
+++ BusinessLogic/Models/Keeper.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Zoo.BusinessLogic.Models.Animals;
+
+namespace Zoo.BusinessLogic.Models
+{
+  public interface IKeeper<in TAnimal> : IKeeper where TAnimal : IAnimal
+  {
+    void StartLookingAfter(TAnimal newAnimal);
+  }
+
+  public class Keeper<TAnimal> : IKeeper<TAnimal> where TAnimal : IAnimal
+  { 
+    private List<TAnimal> animals;
+
+    public Keeper(IEnumerable<TAnimal> animals)
+    {
+      this.animals = new List<TAnimal>(animals);
+    }
+
+    public IEnumerable<T> GetResponsibleAnimals<T>()
+    {
+      return animals.OfType<T>();
+    }
+
+    public void FeedAnimal(Animal animalToFeed)
+    {
+      animalToFeed.Feed();
+    }
+
+    public void GroomAnimal(ICanBeGroomed animalToGroom)
+    {
+      animalToGroom.Groom();
+    }
+
+    public void StartLookingAfter(TAnimal newAnimal)
+    {
+      animals.Add(newAnimal);
+    }
+  }
+}
